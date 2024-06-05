5c5fcdc5f7a30e24a57e7a321d4be60e19f3fbc6
Your Name <you@example.com>
2024-06-05 18:40:59 +0100

    debug

--- src/instruct_1.c
+++ src/instruct_1.c
@@ -1,8 +1,5 @@
 #include "../include/push_swap.h"
 
-// Swap the first 2 elements at the top of stack a.
-// Do nothing if there is only one or no elements.
-
 int  sa_function(int *stake_a, int size_a )
 {
     // Swap the first 2 elements at the top of stack a.
@@ -61,38 +58,3 @@ int pa_function(int *stake_a , int *stake_b , int size_a , int *size_b)
     stake_b[ *size_b - 1] = stake_a[size_a -1];
     return 1; 
 }
-
-int ra_function(int *stake_a , int size_a)
-{
-    //Shift up all elements of stack a by 1.
-    //The first element becomes the last one.
-    int i;
-    int swap;
-
-    i = 0 ;
-    while(i < size_a -1 )
-    {
-        swap = stake_a[i]; 
-        stake_a[i] = stake_a[i + 1] ;
-        stake_a[i + 1] = swap ; 
-        i++;
-    }
-}
-
-int rb_function(int *stake_b , int size_b)
-{
-    //Shift up all elements of stack a by 1.
-    //The first element becomes the last one.
-    int i;
-    int swap;
-
-    i = 0 ;
-    while(i < size_b -1 )
-    {
-        swap = stake_b[i]; 
-        stake_b[i] = stake_b[i + 1] ;
-        stake_b[i + 1] = swap ; 
-        i++;
-    }
-}
-
