87bf6d4a0f31da8533ff689b61baa255fdf297bf
abdo <abdo>
2024-06-05 16:02:12 +0100

    debug

--- src/push_swap_utils_2.c
+++ src/push_swap_utils_2.c
@@ -69,9 +69,8 @@ int ra_function(int *stake_a , int size_a)
     int i;
     int swap;
 
-    i = 0  ; 
-
-    while(i < size_a)
+    i = 0 ;
+    while(i < size_a -1 )
     {
         swap = stake_a[i]; 
         stake_a[i] = stake_a[i + 1] ;
@@ -79,3 +78,21 @@ int ra_function(int *stake_a , int size_a)
         i++;
     }
 }
+
+int rb_function(int *stake_b , int size_b)
+{
+    //Shift up all elements of stack a by 1.
+    //The first element becomes the last one.
+    int i;
+    int swap;
+
+    i = 0 ;
+    while(i < size_b -1 )
+    {
+        swap = stake_b[i]; 
+        stake_b[i] = stake_b[i + 1] ;
+        stake_b[i + 1] = swap ; 
+        i++;
+    }
+}
+
