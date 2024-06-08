5be90bd7884de292d07476fecec6616861581538
Your Name <you@example.com>
2024-06-08 18:17:37 +0100

    debug

--- tests/test.c
+++ tests/test.c
@@ -1,16 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int reverse_rotate_function(int *stake , int  size)
+{
+   //Shift down all elements of stack a by 1.
+   //The last element becomes the first one
+    int i;
+    int swap;
 
-
-  
-
+    i = size -1;
+    while(i > 0 )
+    {
+        swap = stake[i]; 
+        stake[i] = stake[i - 1] ;
+        stake[i - 1] = swap ; 
+        i--;
+    }
+}
 int main(int argc , char **argv)
 {
-   
-   int *tab = malloc(sizeof(int) * 3) ; 
 
-   tab[0] = 0 ;
-   tab[1] = 1;
-   tab[2] = 2;
+   int *tab; 
+
+   tab = malloc(4 * sizeof(int)) ; 
+
+   tab[0] = -1 ;
+   tab[1] =  200;
+   tab[2] = 300;
+   tab[3] =  400 ;
+
+   reverse_rotate_function(tab , 4) ; 
+
+   int i ;
+
+   i = 0 ; 
+
+   while(i < 4)
+   {
+      printf("%d , " , tab[i]) ; 
+      i++;
+   }
+
+
+   
+
+
 
 
 
